4960742aa551464f02e12967110228f9e0cd13d6
vinyciusz <vinycius@outlook.com>
2025-02-16 23:40:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ def home():
     return {"mensagem": "API da IA Jurídica rodando na nuvem!"}
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))  # O Railway define automaticamente a porta correta
+    port = int(os.getenv("PORT", 8000))  # O Railway define essa variável automaticamente
     uvicorn.run(app, host="0.0.0.0", port=port)
